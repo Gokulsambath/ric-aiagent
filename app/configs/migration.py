43aa164aaad2b29cf39c05386c0fe5349a3b8ec3
Gokul <gokulsambath@gmail.com>
2025-12-23 08:59:57 +0000

    feat: add User, Customer, Demo, Email, and AIAgents models to migration script

--- app/configs/migration.py
+++ app/configs/migration.py
@@ -1,6 +1,11 @@
 from app.configs.database import Base, engine
 from app.models.chat_models import ChatSession, ChatMessage
 from app.models.widget_config_model import WidgetConfig
+from app.models.user_model import User
+from app.models.customer_model import Customer
+from app.models.demo_model import Demo
+from app.models.email_model import Email
+from app.models.ollama_model import AIAgents
 
 # ---- Create Database and Tables if not exists ----
 def migrate():
